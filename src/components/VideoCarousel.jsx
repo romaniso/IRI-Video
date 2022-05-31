79d32df21452c038b1bf81e24bfcd6265a25e8f8
Roman Isopenko <isopenkoroman@gmail.com>
2022-05-31 17:14:07 +0200

    fix some issues for deploying on Netlify

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Navigation, Pagination } from "swiper";
 import "swiper/css/bundle";
