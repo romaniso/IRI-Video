a8235ce11bda518b74a7e87179fae460bbf9266b
Roman Isopenko <isopenkoroman@gmail.com>
2023-01-28 22:10:52 +0100

    Restyle AboutSection image, add to it background decor

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -2,28 +2,43 @@ import React from "react";
 import TitleSection from "./TitleSection";
 import styled from "styled-components";
 import DescriptionSection from "./DescriptionSection";
-import AboutImg2 from "../assets/images/Iri-min.jpg";
+import AboutImg2 from "../assets/images/Iri-transparent.png";
 import { Link } from "react-router-dom";
 
 const AboutSectionStyles = styled.div`
   background: rgba(0, 0, 0, 0.9);
+  @keyframes animatePhoto {
+    50% {
+      right: 5px;
+      top: -20px;
+    }
+  }
+  @keyframes animateBg {
+    50% {
+      left: 0%;
+      top: 12%;
+    }
+  }
+
   .about-section__container {
     max-width: 1200px;
-    margin: 4rem auto;
-    /*padding: 17rem 0;*/
+    margin: 0 auto;
+    padding: 5rem 0;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    gap: 2rem;
+    column-gap: 5rem;
     @media only screen and (max-width: 940px) {
-      padding: 10rem 0;
+      padding: 6rem 0 0;
+      row-gap: 2rem;
     }
 
     .left {
       /*border-left: 2px solid var(--highlight);*/
       padding-left: 2rem;
-      flex: 3;
+      /*flex: 3;*/
       text-align: left;
+      max-width: 600px;
       @media only screen and (max-width: 768px) {
         text-align: center;
         padding-left: 3rem;
@@ -41,11 +56,69 @@ const AboutSectionStyles = styled.div`
     }
 
     .right {
-      flex: 3;
-      position: relative;
+      /*flex: 3;*/
+      /*position: relative;*/
       display: flex;
       justify-content: flex-end;
       align-items: center;
+      .imagebox-right {
+        position: relative;
+        max-width: 600px;
+        width: 100%;
+        height: 100%;
+        /*margin: 3rem auto;*/
+        @media only screen and (max-width: 768px) {
+          max-width: 400px;
+          margin-left: 2.2rem;
+        }
+        @media only screen and (max-width: 530px) {
+          max-width: 350px;
+        }
+        &__image {
+          position: relative;
+          top: 0;
+          right: 0;
+          height: 100%;
+          z-index: 20;
+          overflow: hidden;
+          border-radius: 10%;
+          /*animation: animatePhoto 6s linear infinite;*/
+        }
+        &__background {
+          overflow: hidden;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          position: absolute;
+          top: 9%;
+          left: -10%;
+          height: 80%;
+          width: 110%;
+          background-color: var(--highlight);
+          border-radius: 50%;
+          opacity: 80%;
+          animation: animateBg 7s linear infinite;
+          @media only screen and (max-width: 530px) {
+            /*left: 19%;*/
+          }
+          &::before {
+            content: "";
+            position: absolute;
+            width: 150%;
+            height: 150%;
+            background: conic-gradient(
+              from 90deg at 50% 50%,
+              #fff1dc,
+              #fff1dc,
+              #ffcc80,
+              #ff9900,
+              #ffd089,
+              #fff1dc
+            );
+            animation: spinBorder 10s linear infinite;
+          }
+        }
+      }
       img {
         max-width: 420px;
         min-width: 250px;
@@ -84,7 +157,10 @@ export default function AboutSection() {
           </DescriptionSection>
         </div>
         <div className="right">
-          <img src={AboutImg2} alt="me" />
+          <div className="right__imagebox imagebox-right">
+            <img src={AboutImg2} alt="me" className="imagebox-right__image" />
+            <div className="imagebox-right__background"></div>
+          </div>
         </div>
       </div>
     </AboutSectionStyles>
