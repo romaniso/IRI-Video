f61e5e221453a8cbd4a71ae2d5041c0900947555
Roman Isopenko <isopenkoroman@gmail.com>
2023-02-04 22:56:37 +0100

    Add mailer functionality to the contact page, add emailjs library, implement it to the code

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import styled from "styled-components";
 import TitleSection from "../components/TitleSection";
 import VideoBg from "../assets/videos/background-compressed.mp4";
@@ -60,9 +61,40 @@ const ContactPageStyles = styled.div`
     top: 30%;
     transform: translate(-50%, -50%);
   }
+  .section-contact {
+    &__form {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+    }
+  }
 `;
 
 export default function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_yjthayg",
+        "template_rlbnlvc",
+        form.current,
+        "BMi048vbnITw-BuDh"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          e.target.reset();
+          console.log("Message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <ContactPageStyles className="container">
       <div className="video__wrapper">
@@ -71,16 +103,33 @@ export default function Contact() {
         </video>
       </div>
       <TitleSection heading="Contact" subheading="Get in touch with me" />
-      <div className="contact__section">
-        <ContactItem icon={<AiFillPhone />} text="+39 328 617 1688" />
-        <ContactItem
-          icon={<AiTwotoneMail />}
-          text="iri.zinchenko.roma@gmail.com"
-        />
-        <ContactItem text="Rome, Lazio, Italy" />
-        <ContactItem icon={<AiFillFacebook />} text="Facebook" />
-        <ContactItem icon={<AiFillYoutube />} text="YouTube" />
-        <ContactItem icon={<AiFillInstagram />} text="Instagram" />
+      <div className="contact__section section-contact">
+        <div className="section-contact__right">
+          <ContactItem icon={<AiFillPhone />} text="+39 328 617 1688" />
+          <ContactItem
+            icon={<AiTwotoneMail />}
+            text="iri.zinchenko.roma@gmail.com"
+          />
+          <ContactItem text="Rome, Lazio, Italy" />
+          <ContactItem icon={<AiFillFacebook />} text="Facebook" />
+          <ContactItem icon={<AiFillYoutube />} text="YouTube" />
+          <ContactItem icon={<AiFillInstagram />} text="Instagram" />
+        </div>
+        <div className="section-contact__left">
+          <form
+            ref={form}
+            onSubmit={sendEmail}
+            className="section-contact__form"
+          >
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+          </form>
+        </div>
       </div>
     </ContactPageStyles>
   );
